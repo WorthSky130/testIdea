c0227f9e8bbcb99fd85a7a82246471a298feeeef
WorthSky130 <zhaoxuejiaows@sina.com>
2017-10-28 18:18:50 +0800

    edit second

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        String ss = "haohaoni" ;
         System.out.println("Salut luoluo!");
     }
 }
