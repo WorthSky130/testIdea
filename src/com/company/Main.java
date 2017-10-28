c9fa049ca6a29bfd61b3a26b8ab0597d5f33fd56
“WorthSky130 <zhaoxuejiaows@sina.com>
2017-10-28 21:31:08 +0800

    edit four

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
+        //20171028
         String ss = "haohaoni";
         System.out.println("Salut luoluo!"+ss);
     }
