f8327b59b660f66b6dbc1a7e5b0ce3c74ff7ff22
“WorthSky130 <zhaoxuejiaows@sina.com>
2017-10-28 21:16:38 +0800

    edit third

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        String ss = "haohaoni" ;
-        System.out.println("Salut luoluo!");
+        String ss = "haohaoni";
+        System.out.println("Salut luoluo!"+ss);
     }
 }
